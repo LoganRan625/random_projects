246066b02a6501a018f9570e868320677fbc6154
LoganRan625 <75424234+LoganRan625@users.noreply.github.com>
2021-08-16 02:30:51 -0700

    Update macros.asm

--- macros.asm
+++ macros.asm
@@ -1,7 +1,7 @@
 ; use macros like simple function calls
 
 ; add macros to this file and
-; #inlcude <macros.asm> to file you want to use these macros in
+; #include <macros.asm> to file you want to use these macros in
 
 ;section .data
 ;	newline db 10,0 ; call newline after print
